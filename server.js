1a720880ca1c49889e0f47fed596023c44106901
PraveenPriyesh <129766701+PraveenPriyesh@users.noreply.github.com>
2025-02-23 01:58:22 +0530

    all done

---
+++ server.js
@@ -0,0 +1,25 @@
+const express= require('express');
+const app=express();
+app.use(express.json());
+const Port=2000;
+const information=[
+    name1={
+        "Status":"Api created successfully",
+        "Addition infor":"None"
+    },
+    name3={
+        "Status1":"Data2 is working",
+        "Addition infor1":"none"
+    }
+];
+app.get("/api/information",(req,res)=>{
+    res.json(information);
+})
+app.get('/api/information/2',(req,res)=>{
+    res.json(information[1]);
+})
+app.listen(Port,()=>{
+    console.log(`Successfully running on http://localhost:${Port}/api/information`);
+})
+
+    
