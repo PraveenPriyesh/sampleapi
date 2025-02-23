49982e94c21b8ed8d41c16b28b13c9fafbfd12c4
PraveenPriyesh <129766701+PraveenPriyesh@users.noreply.github.com>
2025-02-23 18:52:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express= require('express');
-const app=express();
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// ✅ Allow all origins and headers
+app.use(cors({
+    origin: "*",  // Allow requests from anywhere
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
-app.use(cors());
-const Port=2000;
-const information=require('./data.json');
-app.get("/api/information",(req,res)=>{
+
+const information = require('./data.json');
+
+app.get("/api/information", (req, res) => {
     res.json(information);
-})
-app.get('/api/information/2',(req,res)=>{
+});
+
+app.get('/api/information/2', (req, res) => {
     res.json(information[1]);
-})
-// app.listen(Port,()=>{
-//     console.log(`Successfully running on http://localhost:${Port}/api/information`);
-// })
-module.exports= app
+});
+
+module.exports = app;
